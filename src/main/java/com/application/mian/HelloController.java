a0ee6c600153883d16132821c4772af0937f6cc4
HelloZola <643074224@qq.com>
2020-07-16 22:34:51 +0800

    quartz+spingboot demo初始工程提交

---
+++ src/main/java/com/application/mian/HelloController.java
@@ -0,0 +1,35 @@
+package com.application.mian;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @Autowired
+    QuartzJobManager quartzJobManager;
+
+    @RequestMapping("add")
+    public String add(HttpServletRequest request) {
+
+//        //任务名称
+//        String name = request.getParameter("name");
+//        //任务组名称
+//        String groupName = "task";
+//        //cron表达式
+//        String cron = "0 0/1 * * * ?";
+//        //需要给任务携带的参数
+//        Map<String, Object> map = new HashMap<>();
+//        map.put("name", "张三");
+//        map.put("sex", "0");
+//        quartzJobManager.addJob(HiJob.class, "HiJob", groupName, cron, map);
+
+        return "Hello this is my first springboot demo";
+    }
+}
