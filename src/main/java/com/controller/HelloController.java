6f7156dd6e11dd1e3f6c574ce09eca81425320c1
HelloZola <643074224@qq.com>
2020-07-18 15:32:52 +0800

    debug调试代码修改

--- src/main/java/com/controller/HelloController.java
+++ src/main/java/com/controller/HelloController.java
@@ -26,18 +26,26 @@ public class HelloController {
     @RequestMapping("add")
     public String add(HttpServletRequest request) {
 
-        //任务名称
-//        String name = request.getParameter("name");
-//        //任务组名称
-//        String groupName = "task";
-//        //cron表达式
-//        String cron = "0 0/1 * * * ?";
-//        //需要给任务携带的参数
-//        Map<String, Object> map = new HashMap<>();
-//        map.put("name", "张三");
-//        map.put("sex", "0");
-//        quartzJobManager.addJob(HiJob.class, "HiJob", groupName, cron, map);
+        //创建job
+//        createJobDemo(request);
 
         return "Hello this is my first springboot demo";
     }
+
+
+    private void createJobDemo(HttpServletRequest request) {
+//        任务名称
+        String name = request.getParameter("name");
+        //任务组名称
+        String groupName = "task";
+        //cron表达式
+        String cron = "0 0/1 * * * ?";
+        //需要给任务携带的参数
+        Map<String, Object> map = new HashMap<>();
+        map.put("name", "张三");
+        map.put("sex", "0");
+        quartzJobManager.addJob(HiJob.class, "HiJob", groupName, cron, map);
+    }
+
+
 }
